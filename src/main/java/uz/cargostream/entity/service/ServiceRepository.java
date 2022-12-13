9c755c27e8cbd462e1f8646bb8a116af2d2836ec
Asil7 <asil.biohazard@gmail.com>
2022-12-13 17:50:57 +0500

    New Changes

--- src/main/java/uz/cargostream/entity/service/ServiceRepository.java
+++ src/main/java/uz/cargostream/entity/service/ServiceRepository.java
@@ -3,15 +3,16 @@ package uz.cargostream.entity.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.mail.Service;
 import java.util.List;
 import java.util.UUID;
 
-public interface ServiceRepository extends JpaRepository<Service, UUID> {
+public interface ServiceRepository extends JpaRepository<Services, UUID> {
 
     @Query(nativeQuery = true,
             value = "select ss.services as services\n" +
                     "from services_services ss\n" +
                     "where ss.services_id = :serviceId")
     List<String> getServicesByServiceId(UUID serviceId);
+
+
 }
