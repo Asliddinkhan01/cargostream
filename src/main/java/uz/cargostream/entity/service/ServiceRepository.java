26a419ac1a1b8f01bd8f7e9d6b74d2099a8cfbb7
Asil7 <asil.biohazard@gmail.com>
2022-12-13 17:34:21 +0500

    New Changes

--- src/main/java/uz/cargostream/entity/service/ServiceRepository.java
+++ src/main/java/uz/cargostream/entity/service/ServiceRepository.java
@@ -3,6 +3,7 @@ package uz.cargostream.entity.service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.mail.Service;
 import java.util.List;
 import java.util.UUID;
 
